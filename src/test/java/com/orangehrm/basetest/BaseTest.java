5d01173d399023f76fe0e678b40202171862e341
venu <venu@venu-PC>
2018-12-27 18:46:58 +0530

    New commit

--- src/test/java/com/orangehrm/basetest/BaseTest.java
+++ src/test/java/com/orangehrm/basetest/BaseTest.java
@@ -2,7 +2,6 @@ package com.orangehrm.basetest;
 
 
 import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
